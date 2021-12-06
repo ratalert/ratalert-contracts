6134ffac399462442010cfeeb7a15b07dcae5557
juggernod <juggernod@protonmail.com>
2021-12-06 23:18:42 +0100

    Fix deployment: KitchenPack now is a controller

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -5,9 +5,9 @@ const ChefRat = artifacts.require('ChefRat');
 const KitchenPack = artifacts.require('KitchenPack');
 
 module.exports = async (deployer) => {
-    const foo = await deployer.deploy(FastFood);
+    await deployer.deploy(FastFood);
     const fastFood = await FastFood.deployed();
-    // const fastFood = await deployProxy(FastFood, { deployer });
     const chefRat = await deployProxy(ChefRat, { deployer });
-    await deployProxy(KitchenPack, [chefRat.address, fastFood.address], { deployer });
+    const kitchenPack = await deployProxy(KitchenPack, [chefRat.address, fastFood.address], { deployer });
+    fastFood.addController(kitchenPack.address);
 };
