06b3724a97cf8f8dc94e9fede6f35c9d8bb2b08a
juggernod <juggernod@protonmail.com>
2022-03-21 23:51:11 +0100

    Config update

---
+++ migrations/9_update_contracts.js
@@ -0,0 +1,33 @@
+const Config = require('../config');
+const {deployProxy} = require("@openzeppelin/truffle-upgrades");
+
+const Properties = artifacts.require('Properties');
+const Paywall = artifacts.require('Paywall');
+const Character = artifacts.require('Character');
+const KitchenShop = artifacts.require('KitchenShop');
+const McStake = artifacts.require('McStake');
+const TheStakehouse = artifacts.require('TheStakehouse');
+const LeStake = artifacts.require('LeStake');
+const Gym = artifacts.require('Gym');
+
+module.exports = async (deployer, network) => {
+    const config = Config(network);
+
+    const properties = await Properties.deployed();
+    const paywall = await Paywall.deployed();
+    const character = await Character.deployed();
+    const kitchenShop = await KitchenShop.deployed();
+    const mcStake = await McStake.deployed();
+    const theStakehouse = await TheStakehouse.deployed();
+    const leStake = await LeStake.deployed();
+    const gym = await Gym.deployed();
+
+    await properties.configure(...config.properties);
+    await paywall.configure(...config.payWall);
+    await character.configure(...config.character);
+    await kitchenShop.configure(...config.kitchenShop);
+    await mcStake.configure(      config.kitchen.mcStake.foodTokenMaxSupply,       [config.kitchen.dailyChefEarnings, config.kitchen.ratTheftPercentage, config.kitchen.vestingPeriod, config.kitchen.accrualPeriod], config.kitchen.mcStake.propertyIncrements,                                                                                           config.kitchen.chefEfficiencyMultiplier, config.kitchen.ratEfficiencyMultiplier, config.kitchen.ratEfficiencyOffset, config.kitchen.maxClaimsPerTx);
+    await theStakehouse.configure(config.kitchen.theStakehouse.foodTokenMaxSupply, [config.kitchen.dailyChefEarnings, config.kitchen.ratTheftPercentage, config.kitchen.vestingPeriod, config.kitchen.accrualPeriod], config.kitchen.theStakehouse.propertyIncrements, config.kitchen.theStakehouse.minEfficiency, config.kitchen.chefsPerKitchen,    config.kitchen.chefEfficiencyMultiplier, config.kitchen.ratEfficiencyMultiplier, config.kitchen.ratEfficiencyOffset, config.kitchen.maxClaimsPerTx);
+    await leStake.configure(      config.kitchen.leStake.foodTokenMaxSupply,       [config.kitchen.dailyChefEarnings, config.kitchen.ratTheftPercentage, config.kitchen.vestingPeriod, config.kitchen.accrualPeriod], config.kitchen.leStake.propertyIncrements,       config.kitchen.leStake.minEfficiency,       config.kitchen.chefsPerKitchen,    config.kitchen.chefEfficiencyMultiplier, config.kitchen.ratEfficiencyMultiplier, config.kitchen.ratEfficiencyOffset, config.kitchen.maxClaimsPerTx);
+    await gym.configure(...config.gym);
+};
