0b379a42ac68a318fb87a4678cd7eaed377d2d9e
juggernod <juggernod@protonmail.com>
2022-04-15 13:05:06 +0200

    Base64 library now available as OpenZeppelin dependency

--- contracts/KitchenShop.sol
+++ contracts/KitchenShop.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 import "./GenericPausable.sol";
 import "./FastFood.sol";
 import "./CasualFood.sol";
@@ -128,11 +129,11 @@ contract KitchenShop is Initializable, OwnableUpgradeable, GenericPausable, ERC1
         '"name":"', kitchen.name, '",',
         '"description":"RatAlert, the NFT game that lets you Train2Earn your characters on-chain for higher rewards! https://ratalert.com/",',
         '"external_url":"https://ratalert.com/kitchens/', tokenId.toString(), '",',
-        '"image":"data:image/png;base64,', base64(bytes(drawSVG(bytes(kitchen.png)))), '"',
+        '"image":"data:image/png;base64,', Base64.encode(bytes(drawSVG(bytes(kitchen.png)))), '"',
       '}'
     ));
 
-    return string(abi.encodePacked("data:application/json;base64,", base64(bytes(metadata))));
+    return string(abi.encodePacked("data:application/json;base64,", Base64.encode(bytes(metadata))));
   }
 
   /**
@@ -160,42 +161,5 @@ contract KitchenShop is Initializable, OwnableUpgradeable, GenericPausable, ERC1
       kitchen.name,
       kitchen.png
     );
-  }
-
-  /**
-   * base64 implementation - Written by Brech Devos
-   */
-  string internal constant TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-  function base64(bytes memory data) internal pure returns (string memory) {
-    if (data.length == 0) return '';
-    string memory table = TABLE; // load the table into memory
-    uint256 encodedLen = 4 * ((data.length + 2) / 3); // multiply by 4/3 rounded up
-    string memory result = new string(encodedLen + 32); // add some extra buffer at the end required for the writing
-    assembly {
-      mstore(result, encodedLen) // set the actual output length
-      let tablePtr := add(table, 1) // prepare the lookup table
-      let dataPtr := data // input ptr
-      let endPtr := add(dataPtr, mload(data))
-      let resultPtr := add(result, 32) // result ptr, jump over length
-      for {} lt(dataPtr, endPtr) {} // run over the input, 3 bytes at a time
-      {
-        dataPtr := add(dataPtr, 3)
-        let input := mload(dataPtr) // read 3 bytes
-        // write 4 characters
-        mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F)))))
-        resultPtr := add(resultPtr, 1)
-        mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F)))))
-        resultPtr := add(resultPtr, 1)
-        mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr( 6, input), 0x3F)))))
-        resultPtr := add(resultPtr, 1)
-        mstore(resultPtr, shl(248, mload(add(tablePtr, and(        input,  0x3F)))))
-        resultPtr := add(resultPtr, 1)
-      }
-      // padding with '='
-      switch mod(mload(data), 3)
-      case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
-      case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
-    }
-    return result;
   }
 }
--- contracts/Traits.sol
+++ contracts/Traits.sol
@@ -6,6 +6,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Base64.sol";
 import "./ITraits.sol";
 import "./ICharacter.sol";
 
@@ -60,14 +61,14 @@ contract Traits is Initializable, OwnableUpgradeable, ITraits {
       '"name":"', s.isChef ? 'Chef #' : 'Rat #', tokenId.toString(), '",',
       '"description":"RatAlert, the NFT game that lets you Train2Earn your characters on-chain for higher rewards! https://ratalert.com/",',
       '"external_url":"https://ratalert.com/characters/', tokenId.toString(), '",',
-      '"image":"data:image/svg+xml;base64,', base64(bytes(drawSVG(tokenId))), '",',
+      '"image":"data:image/svg+xml;base64,', Base64.encode(bytes(drawSVG(tokenId))), '",',
       '"attributes":', getAttributes(tokenId),
       '}'
     ));
 
     return string(abi.encodePacked(
       "data:application/json;base64,",
-      base64(bytes(metadata))
+      Base64.encode(bytes(metadata))
     ));
   }
 
@@ -210,42 +211,5 @@ contract Traits is Initializable, OwnableUpgradeable, ITraits {
         traits[i].png
       );
     }
-  }
-
-  /**
-   * base64 implementation - Written by Brech Devos
-   */
-  string internal constant TABLE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
-  function base64(bytes memory data) internal pure returns (string memory) {
-    if (data.length == 0) return '';
-    string memory table = TABLE; // load the table into memory
-    uint256 encodedLen = 4 * ((data.length + 2) / 3); // multiply by 4/3 rounded up
-    string memory result = new string(encodedLen + 32); // add some extra buffer at the end required for the writing
-    assembly {
-      mstore(result, encodedLen) // set the actual output length
-      let tablePtr := add(table, 1) // prepare the lookup table
-      let dataPtr := data // input ptr
-      let endPtr := add(dataPtr, mload(data))
-      let resultPtr := add(result, 32) // result ptr, jump over length
-      for {} lt(dataPtr, endPtr) {} // run over the input, 3 bytes at a time
-      {
-        dataPtr := add(dataPtr, 3)
-        let input := mload(dataPtr) // read 3 bytes
-        // write 4 characters
-        mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(18, input), 0x3F)))))
-        resultPtr := add(resultPtr, 1)
-        mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr(12, input), 0x3F)))))
-        resultPtr := add(resultPtr, 1)
-        mstore(resultPtr, shl(248, mload(add(tablePtr, and(shr( 6, input), 0x3F)))))
-        resultPtr := add(resultPtr, 1)
-        mstore(resultPtr, shl(248, mload(add(tablePtr, and(        input,  0x3F)))))
-        resultPtr := add(resultPtr, 1)
-      }
-      // padding with '='
-      switch mod(mload(data), 3)
-      case 1 { mstore(sub(resultPtr, 2), shl(240, 0x3d3d)) }
-      case 2 { mstore(sub(resultPtr, 1), shl(248, 0x3d)) }
-    }
-    return result;
   }
 }
