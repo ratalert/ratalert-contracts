001dd293bbc8cfea22805bd5e4946011b1d01f79
juggernod <juggernod@protonmail.com>
2022-05-11 08:08:13 +0200

    Whitelist only in development

--- migrations/6_setup_whitelist.js
+++ migrations/6_setup_whitelist.js
@@ -1,13 +1,18 @@
 const Paywall = artifacts.require('Paywall');
+const Config = require('../config');
 
-module.exports = async (deployer) => {
-  const whitelist = (process.env.WHITELIST || '').split(' ').filter(item => item.length === 42);
-  const freeMints = (process.env.FREEMINTS || '').split(' ').filter(item => item.length === 42);
-  const paywall = await Paywall.deployed();
-  if (whitelist.length) {
-    await paywall.addToWhitelist(whitelist);
-  }
-  if (freeMints.length) {
-    await paywall.addToFreeMints(freeMints);
+module.exports = async (deployer, network, accounts) => {
+  if (network === 'development') {
+    const config = Config(network, accounts);
+
+    const whitelist = (process.env.WHITELIST || '').split(' ').filter(item => item.length === 42);
+    const freeMints = (process.env.FREEMINTS || '').split(' ').filter(item => item.length === 42);
+    const paywall = await Paywall.deployed();
+    if (whitelist.length) {
+      await paywall.addToWhitelist(whitelist, { from: config.dao.address });
+    }
+    if (freeMints.length) {
+      await paywall.addToFreeMints(freeMints, { from: config.dao.address });
+    }
   }
 };
